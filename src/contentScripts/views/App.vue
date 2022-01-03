d4adab145a7535f90a108844aee51f82bcabbedd
kawamataryo <ba068082@gmail.com>
2022-01-04 08:20:37 +0900

    feat: add fade animation

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,101 +1,102 @@
 <template>
-  <div v-if="showModal" class="fixed z-99999 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-    <div class="position relative min-h-screen">
-      <div class="fixed inset-0 bg-black bg-opacity-60 transition-opacity" aria-hidden="true" @click="onCloseModal"></div>
-      <div class="absolute align-bottom bg-white rounded-5px text-left overflow-hidden shadow-xl transform transition-all -translate-x-1/2 left-1/2 top-10vh w-500px max-w-screen-80vw">
-        <div class="relative text-gray-600 focus-within:text-gray-400">
-          <div class="p-20px pb-0">
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              class="absolute  w-20px h-20px pl-12px pt-10px"
-            ><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-            <input
-              id="username"
-              ref="searchInput"
-              v-model="searchWord"
-              class="shadow appearance-none border border-gray-400 rounded-5px w-full py-12px px-12px text-gray-700 leading-tight focus:outline-none focus:shadow-outline box-border bg-white pl-43px"
-              type="search"
-              placeholder="Search for.."
-              autofocus
-              @keypress.ctrl.enter.exact.prevent.="onEnterWithControl"
-              @keydown.down.prevent="onArrowDown"
-              @keydown.up.prevent="onArrowUp"
-              @keypress.enter.exact.prevent="onEnter"
-              @keydown.ctrl.n.prevent="onArrowDown"
-              @keydown.ctrl.p.prevent="onArrowUp"
-              @keydown.esc.prevent="onCloseModal"
-            >
-            <nav class="m overflow-scroll max-h-50vh" role="navigation">
-              <template v-if="searchResult.length">
-                <ul class="pl-0">
-                  <li
-                    v-for="(item, i) in searchResult"
-                    :key="i"
-                    :ref="el => { if (el) searchResultRefs[i] = el }"
-                    :aria-selected="i === selectedNumber"
-                    class="block rounded-5px"
-                    :class="{ 'bg-blue-200': i === selectedNumber }"
-                    role="option"
-                  >
-                    <a :href="item.url" class="p-6px block text-13px flex items-center text-black hover:no-underline no-underline">
-                      <img :src="`https://www.google.com/s2/favicons?domain=${item.hostname}`" alt="" class="w-16px h-16px mr-8px inline-block" /><span class="overflow-hidden display-block whitespace-nowrap text-over overflow-ellipsis">{{ item.title }}</span></a>
-                  </li>
-                </ul>
-              </template>
-              <template v-else>
-                <template v-if="searchWord">
+  <transition name="fade">
+    <div v-if="showModal" class="fixed z-99999 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div class="position relative min-h-screen">
+        <div class="fixed inset-0 bg-black bg-opacity-60 transition-opacity" aria-hidden="true" @click="onCloseModal"></div>
+        <div class="absolute align-bottom bg-white rounded-5px text-left overflow-hidden shadow-xl transform transition-all -translate-x-1/2 left-1/2 top-10vh w-560px max-w-screen-80vw">
+          <div class="relative text-gray-600 focus-within:text-gray-400">
+            <div class="p-20px pb-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                class="absolute  w-20px h-20px pl-12px pt-10px"
+              ><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+              <input
+                id="username"
+                ref="searchInput"
+                v-model="searchWord"
+                class="shadow appearance-none border border-gray-400 rounded-5px w-full py-12px px-12px text-gray-700 leading-tight focus:outline-none focus:shadow-outline box-border bg-white pl-43px"
+                type="search"
+                placeholder="Search for.."
+                @keypress.ctrl.enter.exact.prevent.="onEnterWithControl"
+                @keydown.down.prevent="onArrowDown"
+                @keydown.up.prevent="onArrowUp"
+                @keypress.enter.exact.prevent="onEnter"
+                @keydown.ctrl.n.prevent="onArrowDown"
+                @keydown.ctrl.p.prevent="onArrowUp"
+                @keydown.esc.prevent="onCloseModal"
+              >
+              <nav class="m overflow-scroll max-h-50vh" role="navigation">
+                <template v-if="searchResult.length">
                   <ul class="pl-0">
                     <li
-                      :ref="el => { if (el) searchResultRefs[0] = el }"
-                      :aria-selected="true"
+                      v-for="(item, i) in searchResult"
+                      :key="i"
+                      :ref="el => { if (el) searchResultRefs[i] = el }"
+                      :aria-selected="i === selectedNumber"
                       class="block rounded-5px"
-                      :class="{ 'bg-blue-200': true }"
+                      :class="{ 'bg-blue-200': i === selectedNumber }"
                       role="option"
                     >
-                      <a :href="`https://www.google.com/search?q=${searchWord}`" class="p-6px block text-13px flex items-center text-black hover:no-underline no-underline">
-                        <img :src="`https://www.google.com/s2/favicons?domain=google.com`" alt="" class="w-16px h-16px mr-8px inline-block" /><span class="overflow-hidden display-block whitespace-nowrap text-over overflow-ellipsis">"{{ searchWord }}" search with google</span></a>
+                      <a :href="item.url" class="p-6px block text-13px flex items-center text-black hover:no-underline no-underline">
+                        <img :src="`https://www.google.com/s2/favicons?domain=${item.hostname}`" alt="" class="w-16px h-16px mr-8px inline-block" /><span class="overflow-hidden display-block whitespace-nowrap text-over overflow-ellipsis">{{ item.title }}</span></a>
                     </li>
                   </ul>
                 </template>
                 <template v-else>
-                  <div class="flex min-h-15vh w-full justify-center items-center flex-row opacity-40">
-                    <Logo width="50" height="50" class="mr-15px" />
-                    <p class="text-16px text-black font-bold leading-26px">
-                      Fussy<br />History Search
-                    </p>
-                  </div>
+                  <template v-if="searchWord">
+                    <ul class="pl-0">
+                      <li
+                        :ref="el => { if (el) searchResultRefs[0] = el }"
+                        :aria-selected="true"
+                        class="block rounded-5px"
+                        :class="{ 'bg-blue-200': true }"
+                        role="option"
+                      >
+                        <a :href="`https://www.google.com/search?q=${searchWord}`" class="p-6px block text-13px flex items-center text-black hover:no-underline no-underline">
+                          <img :src="`https://www.google.com/s2/favicons?domain=google.com`" alt="" class="w-16px h-16px mr-8px inline-block" /><span class="overflow-hidden display-block whitespace-nowrap text-over overflow-ellipsis">"{{ searchWord }}" search with google</span></a>
+                      </li>
+                    </ul>
+                  </template>
+                  <template v-else>
+                    <div class="flex min-h-15vh w-full justify-center items-center flex-row opacity-40">
+                      <Logo width="50" height="50" class="mr-15px" />
+                      <p class="text-16px text-black font-bold leading-26px">
+                        Fussy<br />History Search
+                      </p>
+                    </div>
+                  </template>
                 </template>
-              </template>
-            </nav>
-          </div>
-          <div class="flex border border-gray-200 h-40px justify-between px-20px border-solid items-center text-11px text-gray-500">
-            <div class="flex">
-              <p class="mr-10px">
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">↑</span>
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block">↓</span>
-                or
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrl + n</span>
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrol + p</span>
-                Navigate,
-              </P>
-              <p class="mr-10px">
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Enter</span>Open,
-              </p>
-              <P>
-                <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrl + Enter</span>
-                Open in new tab
-              </p>
+              </nav>
+            </div>
+            <div class="flex border border-gray-200 h-40px justify-between px-20px border-solid items-center text-11px text-gray-500">
+              <div class="flex">
+                <p class="mr-10px">
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">↑</span>
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block">↓</span>
+                  or
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrl + n</span>
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrol + p</span>
+                  Navigate,
+                </p>
+                <p class="mr-10px">
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Enter</span>Open,
+                </p>
+                <p>
+                  <span class="bg-gray-200 rounded-3px px-5px py-4px inline-block mr-3px">Ctrl + Enter</span>
+                  Open in new tab
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts" setup>
@@ -176,3 +177,12 @@ const onArrowUp = () => {
   }
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s ease;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
