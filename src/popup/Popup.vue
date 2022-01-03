145d5093684b5f592358149e58d31d647ea2198e
kawamataryo <ba068082@gmail.com>
2022-01-04 08:58:56 +0900

    fix: add start command

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,9 +1,16 @@
 <template>
-  <main class="w-[180px] px-4 py-5 text-center text-gray-800 dark:bg-gray-700 dark:text-white">
-    <Logo width="40" height="40" class="inline-block" :dark="isDark" />
-    <p class="mt-2 opacity-50">
-      Fussy history search
-    </p>
+  <main class="w-[190px] px-4 py-5">
+    <div class="flex min-h-15vh w-full justify-center items-center flex-row opacity-80">
+      <Logo width="40" height="40" class="mr-10px" />
+      <p class="text-14px text-black font-bold leading-20px">
+        Fussy<br />History Search
+      </p>
+    </div>
+    <div class="text-center mt-10px">
+      <span class="bg-gray-200 rounded-3px px-8px py-4px inline-block mr-3px">Option(Alt)</span>
+      +
+      <span class="bg-gray-200 rounded-3px px-8px py-4px inline-block mr-3px">f</span>
+    </div>
     <!-- TODO enable dark mode -->
     <!--    <div class="mt-2">-->
     <!--      <IconMoon v-if="isDark" width="40" height="40" class="cursor-pointer" @click="toggleMode" />-->
