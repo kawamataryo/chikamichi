6d8ef11e067c2025810e1caa470603f0ec7bb78e
kawamataryo <ba068082@gmail.com>
2022-01-10 11:52:42 +0900

    chore: update transition time

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -239,7 +239,7 @@ const onArrowUp = () => {
 
 <style scoped>
 .fade-enter-active, .fade-leave-active {
-  transition: opacity .3s ease;
+  transition: opacity .1s ease;
 }
 .fade-enter-from, .fade-leave-to {
   opacity: 0;
