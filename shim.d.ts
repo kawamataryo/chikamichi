b5dba50e7e120396d7cf3547ba101f0a24f0cea1
kawamataryo <ba068082@gmail.com>
2022-01-23 17:04:33 +0900

    clean: remove unused interface

--- shim.d.ts
+++ shim.d.ts
@@ -1,12 +1,9 @@
-import { ProtocolWithReturn } from 'webext-bridge'
+import 'webext-bridge'
 
 declare module 'webext-bridge' {
   export interface ProtocolMap {
     // define message protocol types
     // see https://github.com/antfu/webext-bridge#type-safe-protocols
-    'tab-prev': { title: string | undefined }
-    'get-current-tab': ProtocolWithReturn<{ tabId: number }, { title?: string }>
-    'history-search': { result: string | undefined }
     'change-current-tab': { tabId: number }
     'update-current-page': { url: string }
     'open-new-tab-page': { url: string }
