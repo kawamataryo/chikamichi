a20389a9a87bd29bfc781f30f413333d92b4140d
Adem Kouki <ademkingnew@gmail.com>
2022-02-03 01:53:22 +0100

    Fix overflow-scroll

--- src/components/PageSetting.vue
+++ src/components/PageSetting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full w-full flex flex-col p-20px overflow-scroll dark:text-gray-200 grid grid-cols-4 grid-rows-2 gap-4">
+  <div class="h-full w-full flex flex-col p-20px overflow-hidden dark:text-gray-200 grid grid-cols-4 grid-rows-2 gap-4">
     <div class="border-gray-400 border-1px rounded-5px p-20px col-span-2 col-start-1">
       <h1 class="text-lg">
         🔍 Default search prefix
