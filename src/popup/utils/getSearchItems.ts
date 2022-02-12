7238a2c9f167102b9294a6c3d8998cc8d6d3a9bb
kawamataryo <ba068082@gmail.com>
2022-02-12 19:53:37 +0900

    feat: enable search by parent folder name of bookmarks

--- src/popup/utils/getSearchItems.ts
+++ src/popup/utils/getSearchItems.ts
@@ -1,9 +1,9 @@
 import { Bookmarks, History, Tabs } from 'webextension-polyfill'
 import { SEARCH_ITEM_TYPE } from '~/constants'
 
-const faviconUrl = (url: string) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`
+export const faviconUrl = (url: string) => `https://www.google.com/s2/favicons?domain=${new URL(url).hostname}`
 
-const convertToSearchItemsFromHistories = (histories: History.HistoryItem[]): SearchItem[] => {
+export const convertToSearchItemsFromHistories = (histories: History.HistoryItem[]): SearchItem[] => {
   return histories.filter((history) => {
     // remove google search's history
     if (/google\..+\/search/.test(history.url!))
@@ -17,24 +17,29 @@ const convertToSearchItemsFromHistories = (histories: History.HistoryItem[]): Se
     title: history.title!,
     faviconUrl: faviconUrl(history.url!),
     type: SEARCH_ITEM_TYPE.HISTORY,
+    folderName: '',
   }))
 }
 
-const convertToSearchItemsFromBookmarks = (bookmarkTreeNodes: Bookmarks.BookmarkTreeNode[]): SearchItem[] => {
+export const convertToSearchItemsFromBookmarks = (bookmarkTreeNodes: Bookmarks.BookmarkTreeNode[]): SearchItem[] => {
   // FIXME: want to rewrites it into a clean recursive function that doesn't use side effects.
   const result: SearchItem[] = []
-  const getTitleAndUrl = (bookmarkTreeNodes: Bookmarks.BookmarkTreeNode[]) => {
+  const getTitleAndUrl = (bookmarkTreeNodes: Bookmarks.BookmarkTreeNode[], folderName = '') => {
     bookmarkTreeNodes.forEach((node) => {
-      if (node.children) {
-        getTitleAndUrl(node.children)
+      if (node.type !== 'bookmark' && node.children) {
+        getTitleAndUrl(node.children, node.title)
         return
       }
 
+      if (!node.url)
+        return
+
       result.push({
-        url: node.url!,
+        url: node.url,
         title: node.title,
-        faviconUrl: faviconUrl(node.url!),
+        faviconUrl: faviconUrl(node.url),
         type: SEARCH_ITEM_TYPE.BOOKMARK,
+        folderName,
       })
     })
   }
@@ -42,13 +47,14 @@ const convertToSearchItemsFromBookmarks = (bookmarkTreeNodes: Bookmarks.Bookmark
   return result
 }
 
-const convertToSearchItemsFromTabs = (tabs: Tabs.Tab[]): SearchItem[] => {
+export const convertToSearchItemsFromTabs = (tabs: Tabs.Tab[]): SearchItem[] => {
   return tabs.map(tab => ({
     url: tab.url!,
     title: tab.title!,
     faviconUrl: faviconUrl(tab.url!),
     type: SEARCH_ITEM_TYPE.TAB,
     tabId: tab.id,
+    folderName: '',
   }))
 }
 
